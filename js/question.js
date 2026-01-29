665e6812b69d066c646e906cd274f8fdcaa030a1
tarryhappor <s1b.33310@gmail.com>
2026-01-30 00:41:06 +0900

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,5 +1,16 @@
 let index = 0;
 
+let score = {
+  E: 0,
+  I: 0,
+  S: 0,
+  N: 0,
+  T: 0,
+  F: 0,
+  J: 0,
+  P: 0
+};
+
 const questionEl = document.getElementById("question");
 const countEl = document.getElementById("count");
 
@@ -9,11 +20,16 @@ function show() {
 }
 
 function answer() {
+  const type = questions[index].type;
+  score[type]++;
+
   index++;
 
   if (index < questions.length) {
     show();
   } else {
+    // 結果を保存
+    localStorage.setItem("parkScore", JSON.stringify(score));
     location.href = "result.html";
   }
 }
