3474a476ae969b95188bad0ebc6be71da08fe464
tarryhappor <s1b.33310@gmail.com>
2026-01-30 00:28:54 +0900

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,14 @@
 let i = 0;
 
 let score = {
-  E:0,I:0,S:0,N:0,T:0,F:0,J:0,P:0
+  E: 0,
+  I: 0,
+  S: 0,
+  N: 0,
+  T: 0,
+  F: 0,
+  J: 0,
+  P: 0
 };
 
 const text = document.getElementById("text");
@@ -9,19 +16,18 @@ const count = document.getElementById("count");
 
 function show() {
   text.innerText = questions[i].text;
-  count.innerText = `Q${i+1} / 16`;
+  count.innerText = "Q" + (i + 1) + " / 16";
 }
 
 function answer(type) {
   const q = questions[i];
-  score[q[type]]++;
+  score[q[type]] = score[q[type]] + 1;
 
   i++;
 
   if (i < questions.length) {
     show();
   } else {
-    localStorage.setItem("score", JSON.stringify(score));
     location.href = "result.html";
   }
 }
